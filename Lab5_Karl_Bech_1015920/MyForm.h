da3f31c7761e220aabd0444db7d5aadce14a56d6
martinbech <70785044+martinbechfurlan@users.noreply.github.com>
2020-11-09 09:53:42 -0600

    Update MyForm.h

--- Lab5_Karl_Bech_1015920/MyForm.h
+++ Lab5_Karl_Bech_1015920/MyForm.h
@@ -57,14 +57,14 @@ namespace Lab5KarlBech1015920 {
 	private: System::Windows::Forms::Label^ res_lbl;
 
 	private: System::Windows::Forms::Button^ boton1_btn;
-	private: System::Windows::Forms::Button^ boton2_btn;
-	private: System::Windows::Forms::Label^ modo_lbl;
 
 
 
-	private: System::Windows::Forms::RichTextBox^ rtxt_Arreglo_Binario;
-	private: System::Windows::Forms::Label^ lbl_Secuencial;
-	private: System::Windows::Forms::Label^ lbl_Binario;
+
+
+
+
+
 	private: System::Windows::Forms::Label^ lbl_Tiempo_Titulo;
 	private: System::Windows::Forms::Label^ timer_lbl;
 
@@ -107,11 +107,6 @@ namespace Lab5KarlBech1015920 {
 			this->lbl_Posicion = (gcnew System::Windows::Forms::Label());
 			this->res_lbl = (gcnew System::Windows::Forms::Label());
 			this->boton1_btn = (gcnew System::Windows::Forms::Button());
-			this->boton2_btn = (gcnew System::Windows::Forms::Button());
-			this->modo_lbl = (gcnew System::Windows::Forms::Label());
-			this->rtxt_Arreglo_Binario = (gcnew System::Windows::Forms::RichTextBox());
-			this->lbl_Secuencial = (gcnew System::Windows::Forms::Label());
-			this->lbl_Binario = (gcnew System::Windows::Forms::Label());
 			this->lbl_Tiempo_Titulo = (gcnew System::Windows::Forms::Label());
 			this->timer_lbl = (gcnew System::Windows::Forms::Label());
 			this->boton3_btn = (gcnew System::Windows::Forms::Button());
@@ -150,7 +145,7 @@ namespace Lab5KarlBech1015920 {
 			// 
 			// rtxt_Arreglo
 			// 
-			this->rtxt_Arreglo->Location = System::Drawing::Point(928, 23);
+			this->rtxt_Arreglo->Location = System::Drawing::Point(701, 73);
 			this->rtxt_Arreglo->Margin = System::Windows::Forms::Padding(2);
 			this->rtxt_Arreglo->Name = L"rtxt_Arreglo";
 			this->rtxt_Arreglo->ReadOnly = true;
@@ -166,9 +161,9 @@ namespace Lab5KarlBech1015920 {
 			this->vector_lbl->Location = System::Drawing::Point(611, 76);
 			this->vector_lbl->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->vector_lbl->Name = L"vector_lbl";
-			this->vector_lbl->Size = System::Drawing::Size(116, 13);
+			this->vector_lbl->Size = System::Drawing::Size(48, 13);
 			this->vector_lbl->TabIndex = 4;
-			this->vector_lbl->Text = L"Vector de 1 a 1000";
+			this->vector_lbl->Text = L"Vector:";
 			// 
 			// buscarmetodo_lbl
 			// 
@@ -185,13 +180,12 @@ namespace Lab5KarlBech1015920 {
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"Búsqueda Secuencial", L"Búsqueda Binaria", L"Búsqueda Hash" });
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"Búsqueda Secuencial", L"Búsqueda Binaria", L"Búsqueda buscar" });
 			this->comboBox1->Location = System::Drawing::Point(790, 187);
 			this->comboBox1->Margin = System::Windows::Forms::Padding(2);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(172, 21);
 			this->comboBox1->TabIndex = 6;
-			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::comboBox1_SelectedIndexChanged);
 			// 
 			// lbl_Numero_Buscar
 			// 
@@ -221,7 +215,7 @@ namespace Lab5KarlBech1015920 {
 			// 
 			this->res_lbl->AutoSize = true;
 			this->res_lbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Bold));
-			this->res_lbl->Location = System::Drawing::Point(600, 302);
+			this->res_lbl->Location = System::Drawing::Point(326, 329);
 			this->res_lbl->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->res_lbl->Name = L"res_lbl";
 			this->res_lbl->Size = System::Drawing::Size(23, 13);
@@ -238,63 +232,6 @@ namespace Lab5KarlBech1015920 {
 			this->boton1_btn->Text = L"Continuar";
 			this->boton1_btn->UseVisualStyleBackColor = true;
 			this->boton1_btn->Click += gcnew System::EventHandler(this, &MyForm::boton1_btn_Click);
-			// 
-			// boton2_btn
-			// 
-			this->boton2_btn->Location = System::Drawing::Point(450, 157);
-			this->boton2_btn->Margin = System::Windows::Forms::Padding(2);
-			this->boton2_btn->Name = L"boton2_btn";
-			this->boton2_btn->Size = System::Drawing::Size(91, 25);
-			this->boton2_btn->TabIndex = 12;
-			this->boton2_btn->Text = L"Elegir";
-			this->boton2_btn->UseVisualStyleBackColor = true;
-			this->boton2_btn->Click += gcnew System::EventHandler(this, &MyForm::boton2_btn_Click);
-			// 
-			// modo_lbl
-			// 
-			this->modo_lbl->AutoSize = true;
-			this->modo_lbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->modo_lbl->Location = System::Drawing::Point(536, 331);
-			this->modo_lbl->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
-			this->modo_lbl->Name = L"modo_lbl";
-			this->modo_lbl->Size = System::Drawing::Size(19, 13);
-			this->modo_lbl->TabIndex = 13;
-			this->modo_lbl->Text = L"---";
-			// 
-			// rtxt_Arreglo_Binario
-			// 
-			this->rtxt_Arreglo_Binario->Location = System::Drawing::Point(928, 91);
-			this->rtxt_Arreglo_Binario->Margin = System::Windows::Forms::Padding(2);
-			this->rtxt_Arreglo_Binario->Name = L"rtxt_Arreglo_Binario";
-			this->rtxt_Arreglo_Binario->ReadOnly = true;
-			this->rtxt_Arreglo_Binario->Size = System::Drawing::Size(133, 64);
-			this->rtxt_Arreglo_Binario->TabIndex = 14;
-			this->rtxt_Arreglo_Binario->Text = L"";
-			// 
-			// lbl_Secuencial
-			// 
-			this->lbl_Secuencial->AutoSize = true;
-			this->lbl_Secuencial->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->lbl_Secuencial->Location = System::Drawing::Point(850, 23);
-			this->lbl_Secuencial->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
-			this->lbl_Secuencial->Name = L"lbl_Secuencial";
-			this->lbl_Secuencial->Size = System::Drawing::Size(74, 13);
-			this->lbl_Secuencial->TabIndex = 15;
-			this->lbl_Secuencial->Text = L"Secuencial:";
-			// 
-			// lbl_Binario
-			// 
-			this->lbl_Binario->AutoSize = true;
-			this->lbl_Binario->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->lbl_Binario->Location = System::Drawing::Point(870, 91);
-			this->lbl_Binario->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
-			this->lbl_Binario->Name = L"lbl_Binario";
-			this->lbl_Binario->Size = System::Drawing::Size(50, 13);
-			this->lbl_Binario->TabIndex = 16;
-			this->lbl_Binario->Text = L"Binario:";
 			// 
 			// lbl_Tiempo_Titulo
 			// 
@@ -313,7 +250,7 @@ namespace Lab5KarlBech1015920 {
 			this->timer_lbl->AutoSize = true;
 			this->timer_lbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->timer_lbl->Location = System::Drawing::Point(782, 331);
+			this->timer_lbl->Location = System::Drawing::Point(892, 302);
 			this->timer_lbl->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->timer_lbl->Name = L"timer_lbl";
 			this->timer_lbl->Size = System::Drawing::Size(19, 13);
@@ -342,7 +279,7 @@ namespace Lab5KarlBech1015920 {
 			// searchnum_txt
 			// 
 			this->searchnum_txt->Location = System::Drawing::Point(295, 159);
-			this->searchnum_txt->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10000, 0, 0, 0 });
+			this->searchnum_txt->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 9000000, 0, 0, 0 });
 			this->searchnum_txt->Name = L"searchnum_txt";
 			this->searchnum_txt->Size = System::Drawing::Size(120, 20);
 			this->searchnum_txt->TabIndex = 21;
@@ -398,11 +335,6 @@ namespace Lab5KarlBech1015920 {
 			this->Controls->Add(this->boton3_btn);
 			this->Controls->Add(this->timer_lbl);
 			this->Controls->Add(this->lbl_Tiempo_Titulo);
-			this->Controls->Add(this->lbl_Binario);
-			this->Controls->Add(this->lbl_Secuencial);
-			this->Controls->Add(this->rtxt_Arreglo_Binario);
-			this->Controls->Add(this->modo_lbl);
-			this->Controls->Add(this->boton2_btn);
 			this->Controls->Add(this->boton1_btn);
 			this->Controls->Add(this->res_lbl);
 			this->Controls->Add(this->lbl_Posicion);
@@ -424,14 +356,8 @@ namespace Lab5KarlBech1015920 {
 		}
 #pragma endregion		
 		Stopwatch^ stopWatch = gcnew Stopwatch();
-		
-
 		array<Int32^>^ totnum;
-		array<Int32^>^ vector1;
-		array<Int32^>^ totnum2;
-		array<Int32^>^ hastot;
 		Random^ randomnum = gcnew Random();
-		
 		Hashtable^ hashfunc = gcnew Hashtable();
 		
 		int random;
@@ -439,127 +365,104 @@ namespace Lab5KarlBech1015920 {
 		int eleccion; 
 		int searchnum; 
 		int same = 0;
-		int n; 
-		int r;
+		void bin(array<Int32^>^ arreglo, int numero, int R, int M) {
+
+			//CÓDIGO DEL BINARIO BASADO DE GEEKFORCE https://www.geeksforgeeks.org/binary-search/
+
+
+			if (R >= 1) {
+				// If the element is present at the middle 
+				 // itself 
+				int t = (M + R) / 2;
+				if (same == t) {
+					res_lbl->Text = "No se encontró el número";
+				}
+				// If element is smaller than mid, then 
+				 // it can only be present in left subarray
+				else {
+					same = t;
+					if (Convert::ToInt32(arreglo[t]) == numero) {
+						res_lbl->Text = Convert::ToString(t);
+					}
+					else if (Convert::ToInt32(arreglo[t]) > numero) {
+						bin(arreglo, numero, t - 1, M);
+					}
+					else if (Convert::ToInt32(arreglo[t]) < numero) {
+						bin(arreglo, numero, R, t + 1);
+					}
+				}
+			}
+		}
+		//HASTA AQUÍ EL CÓDIGO DE GEEKFORCE
+
+		int buscar(int numero, int modulo) {
+			int resultado;
+			resultado = numero % modulo;
+			return resultado;
+		}
+		void bubblesort(array<Int32^>^ vector, int quantity) {
+			int aux, i, j;
+			for (i = 0; i < quantity - 1; i++) {
+				for (j = i + 1; j < quantity; j++) {
+					if (Convert::ToInt32(vector[i]) > Convert::ToInt32(vector[j])) {
+						aux = Convert::ToInt32(vector[i]);
+						vector[i] = vector[j];
+						vector[j] = aux;
+					}
+				}
+			}
+		}
+
 	private: System::Void boton1_btn_Click(System::Object^ sender, System::EventArgs^ e) {
-		try {
 			total = Convert::ToInt32(totalnums_txt->Text);
 			totnum = gcnew array<Int32^>(total);
-			totnum2 = gcnew array<Int32^>(total);
-			hastot = gcnew array<Int32^>(total);
+		
 			for (int i = 0; i < total; i++) {
-				random = randomnum->Next(1001);				
+				random = randomnum->Next(9000000);				
 				totnum[i] = random;
-				totnum2[i] = totnum[i];
-				hastot[i] = totnum[i];
 				rtxt_Arreglo->Text += totnum[i] + "\r\n";
 			}
-			ordenar(totnum2, total);
+
+
+			bubblesort(totnum, total);
 			for (int i = 0; i < total; i++) {
-				rtxt_Arreglo_Binario->Text += totnum2[i] + "\r\n";
 			}
 			for (int i = 0; i < total; i++) {
-				if (hashfunc->ContainsKey(Hash(Convert::ToInt32(hastot[i]), 1000))) {
-					n = 1;
-					r = 0;
-					while (hashfunc->ContainsKey(Hash(Convert::ToInt32(hastot[i]) + r, 1000 * n))) {
-						n++;
-						r = r + 1000;
-					}
-					hashfunc->Add(Hash(Convert::ToInt32(hastot[i]) + r, 1000 * n), Convert::ToString(hastot[i]));
+				if (hashfunc->ContainsKey(buscar(Convert::ToInt32(totnum[i]), 9000000))) {
+					hashfunc->ContainsKey(buscar(Convert::ToInt32(totnum[i]) + 1, 9000000));
 
 				}
 				else {
-					hashfunc->Add(Hash(Convert::ToInt32(hastot[i]), 1000), Convert::ToString(hastot[i]));
+					hashfunc->Add(buscar(Convert::ToInt32(totnum[i]), 9000000), Convert::ToString(totnum[i]));
 				}
 			}
 
 			
 		}
-		catch (Exception^ i) {
-			MessageBox::Show("Número incorrecto");
-		}
-	}
-private: System::Void comboBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {	
+		
 	
-}	   
-private: System::Void boton2_btn_Click(System::Object^ sender, System::EventArgs^ e) {
-	try {
-		searchnum = Convert::ToInt32(searchnum_txt->Text);
-		if (searchnum > 0 && searchnum <= 1000) {
-		}
-		else {
-			MessageBox::Show("Rango de número incorrecto");
-		}
-	}
-	catch (Exception^ e) {
-		MessageBox::Show("Número incorrecto");
-	}
-}
-	   void Busqueda_Secuencial(array<Int32^>^ arreglo, int numero) {		   
-		   for (int i = 0; i < total; i++) {
-			   if (Convert::ToInt32(numero) == Convert::ToInt32(arreglo[i])) {
-				   res_lbl->Text = Convert::ToString(i);
-				   break;
-			   }
-			   else {
-				   res_lbl->Text = "Número no dentro del vector";
-			   }
-		   }
-		   res_lbl->Visible = true;
-		   
-	   }
-	   void Busqueda_Binaria(array<Int32^>^ arreglo, int numero, int mayor, int menor) {	
-		   int  contadortemp=0;		   
-		   try {
-			   if (mayor >= 1) {
-				   int temp = (menor + mayor) / 2;
-				   if (same == temp) {
-					   res_lbl->Text = "No se encontró el número";
-				   }
-				   else {
-					   same = temp;
-					   if (Convert::ToInt32(arreglo[temp]) == numero) {
-						   res_lbl->Text = Convert::ToString(temp);
-					   }
-					   else if (Convert::ToInt32(arreglo[temp]) > numero) {
-						   Busqueda_Binaria(arreglo, numero, temp - 1, menor);
-					   }
-					   else if (Convert::ToInt32(arreglo[temp]) < numero) {
-						   Busqueda_Binaria(arreglo, numero, mayor, temp + 1);
-					   }
-				   }
-			   }
-		   }
-		   catch (Exception^ e) {			   
-		   }
-	   }
-	   int Hash(int numero, int modulo) {
-		   int resultado;
-		   resultado = numero % modulo;
-		   return resultado;
-	   }
-	   void ordenar(array<Int32^>^ vector, int quantity) {
-		   int aux, i, j;
-		   for (i = 0; i < quantity - 1; i++) {
-			   for (j = i + 1; j < quantity; j++) {
-				   if (Convert::ToInt32(vector[i]) > Convert::ToInt32(vector[j])) {
-					   aux = Convert::ToInt32(vector[i]);
-					   vector[i] = vector[j];
-					   vector[j] = aux;					   
-				   }
-			   }
-		   }
-	   }
+   
+
+	  
 
 private: System::Void boton3_btn_Click(System::Object^ sender, System::EventArgs^ e) {
+	searchnum = Convert::ToInt32(searchnum_txt->Text);
 	stopWatch->Restart();
 	eleccion = comboBox1->SelectedIndex;
 	if (eleccion == 0) {
 		stopWatch->Start();
 		Thread::Sleep(1000);
-		Busqueda_Secuencial(totnum, searchnum+1-1);
-		modo_lbl->Text = "Arreglo original";
+		for (int i = 0; i < total; i++) {
+			if (Convert::ToInt32(searchnum+1-1) == Convert::ToInt32(totnum[i])) {
+				res_lbl->Text = Convert::ToString(i);
+				break;
+			}
+			else {
+				res_lbl->Text = "Número no dentro del vector";
+			}
+		}
+	
+
 	
 		stopWatch->Stop();
 		TimeSpan^ ts = stopWatch->Elapsed;
@@ -570,8 +473,7 @@ private: System::Void boton3_btn_Click(System::Object^ sender, System::EventArgs
 	else if (eleccion == 1) {
 		stopWatch->Start();
 		Thread::Sleep(1000);
-		Busqueda_Binaria(totnum2, searchnum+1-1, totnum->Length - 1, 0);
-		modo_lbl->Text = "Arreglo ordenado de menor a mayor";
+		bin(totnum, searchnum+1-1, totnum->Length - 1, 0);
 		stopWatch->Stop();
 		TimeSpan^ ts = stopWatch->Elapsed;
 		String^ elapsedTime = String::Format("{0:00}:{1:00}:{2:00}.{3:00}", ts->Hours, ts->Minutes, ts->Seconds, ts->Milliseconds / 10);
@@ -582,10 +484,9 @@ private: System::Void boton3_btn_Click(System::Object^ sender, System::EventArgs
 	else if (eleccion == 2) {
 		stopWatch->Start();
 		Thread::Sleep(1000);
-		int aux = Hash(Convert::ToInt32(searchnum)+1-1, 1000);
+		int aux = buscar(Convert::ToInt32(searchnum)+1-1, 1000);
 		if (hashfunc->ContainsKey(searchnum)) {
 			res_lbl->Text = Convert::ToString(aux);
-			modo_lbl->Text = "Arreglo modificado especialmente con función hash";
 		}
 		else {
 			res_lbl->Text = "No se encontró el número";
